45765b4b47e47fe659055006a48422a85e12f644
jefferson ademir zimmermann <91795294+jeffersoZ@users.noreply.github.com>
2025-05-05 22:01:53 -0300

    Update index.js
    Simplificando o codigo

--- Calculadora/js/index.js
+++ Calculadora/js/index.js
@@ -1,21 +1,6 @@
 const visor = document.getElementById('visor-input');
 
 function inserirValor(valor) {
-    const valorAtual = visor.value;
-    const ultimoCaractere = valorAtual.slice(-1);
-    
-    //trata caso de dois operadores seguidos
-    if (['+', '-', '*', '/', '.'].includes(ultimoCaractere)) {
-        if (['+', '-', '*', '/', '.'].includes(valor)) {
-            return;
-        }
-    }
-    
-    //trata começar com operadores
-    if (valorAtual === '' && ['*', '/', '+', '.'].includes(valor)) {
-        return;
-    }
-
     visor.value += valor;
 }
 
@@ -24,22 +9,5 @@ function limparVisor() {
 }
 
 function calcularResultado() {
-    try {
-        const expressao = visor.value;
-        
-        // Verificar se a expressão está vazia ou termina com operador
-        if (!expressao || ['+', '-', '*', '/', '.'].includes(expressao.slice(-1))) {
-            return;
-        }
-        
-        // Calcular resultado
-        const resultado = eval(expressao);
-        visor.value = resultado;
-    //se der erro exibe a mensagem de erro
-    } catch (error) {
-        visor.value = 'Erro';
-        setTimeout(() => {
-            limparVisor();
-        }, 1000);
-    }
+    visor.value = eval(visor.value);
 }
